0ea3ba603b4c7ab823fb7b1e8a7cb23774a5cabd
Olaf Lesiuk <olafl220@gmail.com>
2025-11-06 16:23:51 +0100

    Finished respawn func

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -868,8 +868,17 @@ void loseLifeAndRespawn(void) {
   myDrawFullRectangle(pacmanPos.col*SQ_SIZE+1, pacmanPos.row*SQ_SIZE+1, SQ_SIZE-1, SQ_SIZE-1, LCD_COLOR_BLACK);
   gameBoard[pacmanPos.row][pacmanPos.col] = 0;
 
+  // odtworz kropke, jezli byla i to nie sciana
+  if (!walls[pacmanPos.row][pacmanPos.col] && visitedFields[pacmanPos.row][pacmanPos.col] == 0) {
+    myDrawPixel(SQ_SIZE*pacmanPos.col + SQ_SIZE/2, SQ_SIZE*pacmanPos.row + SQ_SIZE/2, LCD_COLOR_WHITE);
+  }
 
+  if (livesLeft > 0) livesLeft--;
 
+  // respawn Pac-Mana na losowym wolnym polu, roznym od Pinky
+  do { pacmanPos.row = rand()%NROW; pacmanPos.col = rand()%NCOL; }
+  while (walls[pacmanPos.row][pacmanPos.col] || (pacmanPos.row==pinkyPos.row && pacmanPos.col==pinkyPos.col));
+}
 
 static inline void DoMove(Dir d) {
   switch (d) {
