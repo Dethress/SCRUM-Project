d098f25d1062a9107a551c5b71f33cca10b0436e
gimbus14 <michalmazurek14@gmail.com>
2025-11-19 23:01:25 +0100

    power ups!

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -92,9 +92,9 @@ uint8_t freezeActive = 0;           // 1 = Pinky zamrożony
 uint32_t freezeTimer = 0;           // odlicza 3 sekundy
 uint32_t freezeDuration = 3000; // będzie nadpisane przez difficultyLevel
 
-#define BTN_DEBOUNCE_MS       20U    // filtr drgań styków
-#define BTN_REPEAT_DELAY_MS  160U    // po tyle ms pierwszy powtórzony krok
-#define BTN_REPEAT_MS         80U    // odstęp kolejnych kroków przy trzymaniu
+uint8_t powerupRow = 0;
+uint8_t powerupCol = 0;
+uint8_t powerupExists = 0;          // 1 = power-up leży na planszy
 
 static Dir     g_btn_last = DIR_NONE;        // ostatni stabilny kierunek
 static uint32_t g_btn_last_change_ms = 0;    // kiedy zmieniĹ‚ siÄ™ stan
